ff326f915731253da774aab0074ccb5a97c771a9
Route9Library <michaelssendi302@gmail.com>
2020-07-29 16:32:40 -0400

    simple calculation

---
+++ Calc.js
@@ -0,0 +1,25 @@
+// console.log("Code Differently");
+
+// console.log(12 + 30);
+
+// console.log(5 - 3);
+
+// console.log(3 * 3);
+
+// console.log(12 / 2);
+
+var x = 72;
+
+var y = 8;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+for(var i=5; i<=15; i += 2){
+    console.log(i)
+}
