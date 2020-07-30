d70322d1aa70f1b88693d34d134f7433c35dcb03
Route9Library <michaelssendi302@gmail.com>
2020-07-30 16:28:43 -0400

    if/else statements

--- Calc.js
+++ Calc.js
@@ -8,18 +8,32 @@
 
 // console.log(12 / 2);
 
-var x = 72;
+// var x = 72;
 
-var y = 8;
+// var y = 8;
 
-console.log(x+y);
+// console.log(x+y);
 
-console.log(x-y);
+// console.log(x-y);
 
-console.log(x*y);
+// console.log(x*y);
 
-console.log(x/y);
+// console.log(x/y);
 
-for(var i=5; i<=15; i += 2){
-    console.log(i)
+// Write a program that prints 1-100 Multiples of 3 print "Fizz" Multiples of 5 print "Buzz"
+
+for(var i=1; i<=20; i++){
+    if(i % 3 == 0){
+        console.log("FIzz");
+    } else{
+        console.log("cool coder")
+    }
 }
+
+// for(var i=5; i<=15; i++){
+//     if(i < 7){
+//         console.log("less than");
+//     } else {
+//         console.log("greater than");
+//     }
+// }
